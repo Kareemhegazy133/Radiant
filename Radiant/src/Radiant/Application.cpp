624e8aab3442b23f5c03fc83ab3a81eaed424693
Kareem Hegazy <kthegazy2002@gmail.com>
2024-01-27 15:59:43 -0500

    Added Input polling

--- Radiant/src/Radiant/Application.cpp
+++ Radiant/src/Radiant/Application.cpp
@@ -5,6 +5,8 @@
 
 #include <glad/glad.h>
 
+#include "Input.h"
+
 namespace Radiant {
 
 #define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
@@ -60,6 +62,8 @@ namespace Radiant {
 			for (Layer* layer : m_LayerStack)
 				layer->OnUpdate();
 
+			auto [x, y] = Input::GetMousePosition();
+			RD_CORE_TRACE("{0}, {1}", x, y);
 			m_Window->OnUpdate();
 		}
 	}
