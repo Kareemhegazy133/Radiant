a83b50c8e72fd914992151d7018730718b232a31
Kareem Hegazy <kthegazy2002@gmail.com>
2024-01-29 23:24:51 -0500

    Added OpenGL Buffer abstraction

--- Radiant/src/Radiant/Application.h
+++ Radiant/src/Radiant/Application.h
@@ -10,6 +10,7 @@
 #include "Radiant/ImGui/ImGuiLayer.h"
 
 #include "Radiant/Renderer/Shader.h"
+#include "Radiant/Renderer/Buffer.h"
 
 namespace Radiant {
 
@@ -37,9 +38,10 @@ namespace Radiant {
 		bool m_Running = true;
 		LayerStack m_LayerStack;
 
-		unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
+		unsigned int m_VertexArray;
 		std::unique_ptr<Shader> m_Shader;
-
+		std::unique_ptr<VertexBuffer> m_VertexBuffer;
+		std::unique_ptr<IndexBuffer> m_IndexBuffer;
 	private:
 		static Application* s_Instance;
 	};
---
+++ Radiant/src/Radiant/Renderer/Renderer.cpp
@@ -0,0 +1,7 @@
+#include "rdpch.h"
+#include "Renderer.h"
+
+namespace Radiant {
+
+	RendererAPI Renderer::s_RendererAPI = RendererAPI::OpenGL;
+}
