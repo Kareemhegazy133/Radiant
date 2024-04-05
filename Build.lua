b68006fcacdc65bf0901f856991b4dd3bff75f54
Kareem Hegazy <kthegazy2002@gmail.com>
2024-04-05 00:51:59 -0400

    Initial Commit

---
+++ Build.lua
@@ -0,0 +1,16 @@
+workspace "TheReaper"
+   architecture "x64"
+   configurations { "Debug", "Release", "Dist" }
+   startproject "Game"
+
+   -- Workspace-wide build options for MSVC
+   filter "system:windows"
+      buildoptions { "/EHsc", "/Zc:preprocessor", "/Zc:__cplusplus" }
+
+OutputDir = "%{cfg.system}-%{cfg.architecture}/%{cfg.buildcfg}"
+
+group "Engine"
+	include "Engine/Build-Engine.lua"
+group ""
+
+include "Game/Build-Game.lua"
---
+++ Engine/Source/Engine.cpp
@@ -0,0 +1,13 @@
+#include "Engine.h"
+
+#include <iostream>
+
+namespace Engine {
+
+	void PrintHelloWorld()
+	{
+		std::cout << "Hello World!\n";
+		std::cin.get();
+	}
+
+}
